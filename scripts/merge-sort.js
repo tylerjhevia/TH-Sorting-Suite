46ce70db9929562abf22a3c48849ecb1274b4fa3
Tyler Hevia <tylerhevia@gmail.com>
2017-07-12 14:24:20 -0600

    merge sort, ignore node modules

--- scripts/merge-sort.js
+++ scripts/merge-sort.js
@@ -1,16 +1,29 @@
 const mergeArrays = (array1, array2) => {
   var sortedArray = [];
   while (array1.length > 0 || array2.length > 0) {
-    console.log('array 1: ' + array1, 'array 2: ' + array2,'sorted array: ' + sortedArray);
     if (array1[0] <= array2[0] || array2[0] === undefined) {
       var lower = array1[0];
       array1.shift();
       sortedArray.push(lower);
     } else {
-       var lower = array2[0];
+      var lower = array2[0];
         array2.shift();
         sortedArray.push(lower);
+        console.log('sorted array: ' + sortedArray);
       }
   }
   return sortedArray;
 }
+
+const mergeSort = array => {
+  console.log('array: ' + array);
+  if (array.length < 2) {
+    return array;
+  }
+  while (array.length > 1) {
+    console.log(array);
+    var arrayMid = Math.round(array.length / 2)
+    return mergeArrays(mergeSort(array.slice(0, arrayMid)), mergeSort(array.slice(arrayMid)));
+  }
+}
+export default {mergeArrays, mergeSort};
--- tests/merge-sort-test.js
+++ tests/merge-sort-test.js
@@ -1,3 +1,8 @@
 import { expect } from 'chai';
 import merge from '../scripts/merge-sort.js';
 import arrays from '../scripts/generating-arrays.js';
+
+
+describe('Merge sort', () => {
+  let testArray = [];
+})
