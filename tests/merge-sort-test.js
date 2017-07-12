c2c5ec6a94581bbf8ac3260fcc39ac61e23b11f4
Tyler Hevia <tylerhevia@gmail.com>
2017-07-12 14:29:48 -0600

    remove node modules, update merge-sort test

--- tests/merge-sort-test.js
+++ tests/merge-sort-test.js
@@ -2,7 +2,41 @@ import { expect } from 'chai';
 import merge from '../scripts/merge-sort.js';
 import arrays from '../scripts/generating-arrays.js';
 
-
-describe('Merge sort', () => {
+describe('fillArray', () => {
   let testArray = [];
+
+  it('should fill an array with 100 integers', () => {
+    expect(testArray.length).to.equal(0);
+    arrays.fillArray(testArray);
+    expect(testArray.length).to.equal(100);
+  })
+})
+
+describe('Merge Sort', () => {
+  let testArray;
+
+  beforeEach(() => {
+    testArray = [];
+  })
+
+  it('should start with an array with 100 values', () => {
+    arrays.fillArray(testArray);
+    expect(testArray.length).to.equal(100);
+  })
+
+  it('should sort an array of numbers from lowest to highest', () => {
+    arrays.fillArray(testArray);
+    expect(testArray.length).to.equal(100);
+    merge.mergeSort(testArray);
+    expect(testArray).to.equal(testArray.sort());
+  })
+
+  it('should sort an array of letters into alphabetical order')
+
+  // checking to see if test is comparing array order or array length
+    it('should check arrays for order and not for length', () => {
+      let funArray = [1, 2];
+      funArray = [2, 1];
+      expect(testArray).to.equal(funArray);
+    })
 })
