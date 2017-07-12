a062e59926427a7281b9dea5c6f67b9f77b1f1d2
Tyler Hevia <tylerhevia@gmail.com>
2017-07-12 00:18:16 -0600

    insertion sort function

--- scripts/insertion-sort.js
+++ scripts/insertion-sort.js
@@ -1,4 +1,14 @@
+const insertion = array => {
+  for (let i = 1; i < array.length; i++) {
+    console.log(array);
+    let current = array[i];
+    for (var j = (i - 1); j > -1 && array[j] > current; j-- ) {
+      [array[j + 1], array[j]] = [array[j], array[j + 1]];
+    }
+    array[j + 1] = current;
+  }
+  return array;
+}
 
 
-
-// export default {};
+export default {insertion};
