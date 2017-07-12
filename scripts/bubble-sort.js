a701a89de84d55e7d61eebfcdd17f99a7a408b3a
Tyler Hevia <tylerhevia@gmail.com>
2017-07-11 23:26:19 -0600

    generate letter array

--- scripts/bubble-sort.js
+++ scripts/bubble-sort.js
@@ -11,6 +11,8 @@ const bubble = array => {
   return array
 }
 
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 function fillArray(arr) {
   for (let i = 0; i < 100; i++) {
     arr.push(Math.round(Math.random() * (1000 - 1) + 1));
